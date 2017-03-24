7af8e09d1a7e4e63554a5785de2fdaa33eb4c564
Lehner Joël <lej@ti8m.ch>
2017-03-24 16:42:24 +0100

    added initial version

---
+++ src/main/java/org/camunda/bpm/getstarted/powerteam/HrprocessApplication.java
@@ -0,0 +1,12 @@
+package org.camunda.bpm.getstarted.powerteam;
+
+import org.camunda.bpm.application.ProcessApplication;
+import org.camunda.bpm.application.impl.ServletProcessApplication;
+
+@ProcessApplication("HR Process App")
+public class HrprocessApplication extends ServletProcessApplication {
+	  // empty implementation
+}
+
+
+
